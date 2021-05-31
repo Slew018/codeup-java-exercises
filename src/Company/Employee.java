fd6b1c5a6289fa5220688d1366f2452a7458fc38
Sean Lewis <sean.lewis018@gmail.com>
2021-05-31 10:00:43 -0500

    java practice5

--- src/Company/Employee.java
+++ src/Company/Employee.java
@@ -2,6 +2,7 @@ package Company;
 
 //create an abstract class using the 'abstract' keyword
 //abstract classes cannot be instantiated
-
+// create a subclass from the abstract and then instantiate the subclass
+//instantiation - to create an instance (an object) of that class
 abstract class Employee {
 }
