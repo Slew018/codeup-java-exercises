f9279e454a1a5da7e8e32a0cd559f8d89d84c7b7
Sean Lewis <sean.lewis018@gmail.com>
2021-06-24 21:45:29 -0500

    loops java

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,28 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
+//        MINI EXERCISE
+// TODO: create three unique usernames (user1, user2, user3).
+//  create a fourth user and set it equal to user input. (ask
+//  the user to enter a username). if the username has already
+//  been used by user1, user2, or user3, print "sorry this
+//  username already exists.
+
+
+//TODO: refactor your code from the previous mini exercise into a switch statement
+
+
+//TODO: using previous mini exercise, allow the user to input a new username several times until a unique username is entered.
+public class ControlFlowExercises {
+    public static void main(String[]args){
+
+        int i = 5;
+        while (i <= 15){
+            System.out.println("i is " + i);
+            i++;
+        }
+
+
+    }
+}
