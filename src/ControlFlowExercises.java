a57d732042a2c271dbde0e39201abbc0c65ca01f
Sean Lewis <sean.lewis018@gmail.com>
2021-06-29 12:09:41 -0500

    java warmups

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -15,21 +15,30 @@ import java.util.Scanner;
 
 //TODO: using previous mini exercise, allow the user to input a new username several times until a unique username is entered.
 public class ControlFlowExercises {
-    public static void main(String[]args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number: ");
+        int number = sc.nextInt();
 
-//        int i = 5;
-//        while (i <= 15) {
-//            System.out.println("i is " + i);
-//            i++;
-//        }
-//        for(int i = 5; i = 15; i++){
-//            System.out.println(i);
-//        }
+        if (number % 5 == 0 && number % 3 ==0) {
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 5 == 0) {
+            System.out.println("Buzz");
+        } else
+            System.out.println(number);
+    }
+
+//        for (int x = 5; x <=15; x++){
+//            System.out.println(x);
+//    }
 //
-//            for (int n = 2; n <= 1000000; n++){
-//                double x = 2 * n;
-//                System.out.println(x);
-//            }
-      }
+//            for (int n = 2;n <=1_000_000;n++){
+//                n *= 2;
+//        System.out.println(n);
+//    }
+
 }
+
 
--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,25 +2,43 @@ import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        System.out.println("Who is your favorite basketball team? ");
-        String basketballTeam = sc.next();
-        System.out.println(epsilon(basketballTeam));
-        System.out.println(iota(epsilon(basketballTeam)));
+        String fullName = sc.nextLine();
+        String[] array = fullName.split(" ");
+        System.out.println(epsilon(fullName));
+        System.out.println(epsilon(array[0], array[1]));
+    }
+
+    public static String epsilon(String fullName) {
+        return "Hello " + fullName;
+    }
+
+    public static String epsilon(String firstName, String lastName) {
+        return "Hello " + firstName + " " + lastName;
     }
 
 
-    public static String epsilon(String basketballTeam) {
-        return basketballTeam + " is the best darn rootin' tootin' team on this flat earth!";
-    }
 
-    public static String iota(String basketballTeam) {
-        return "Length = " + basketballTeam.length();
-    }
 }
-//    TODO: create a new method that accepts the name of a basketball
-//     team (ex: the spurs) and returns a String that says
-//     "[basketballTeam] is the best team.". Invoke that method within
-//     your main method (remember to pass in the name of the basketball
-//     team). print out the result of the new method.
+
+//    TODO: invoke a method that accepts (and returns) the following:
+//        - accepts: your first name and your last name. returns: A
+//        string that reads: "Hello Laura Ruiz-Roehrs (replace with your
+//        first and last)";
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Who is your favorite basketball team? ");
+//        String basketballTeam = sc.next();
+//        System.out.println(epsilon(basketballTeam));
+//        System.out.println(iota(epsilon(basketballTeam)));
+//    }
+//
+//
+//    public static String epsilon(String basketballTeam) {
+//        return basketballTeam + " is the best darn rootin' tootin' team on this flat earth!";
+//    }
+//
+//    public static String iota(String basketballTeam) {
+//        return "Length = " + basketballTeam.length();
+//    }
