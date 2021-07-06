6a9e51d5a9d25839635c1db13119f69815752bbe
Sean Lewis <sean.lewis018@gmail.com>
2021-07-06 15:58:31 -0500

    java interface

---
+++ src/Company/TeamLead.java
@@ -0,0 +1,13 @@
+package Company;
+
+public class TeamLead implements TimeSheets {
+
+    @Override
+    public void fillOutTimeSheets() {
+
+//        @Override
+//        public void firePeople() {
+//            System.out.println();
+//        }
+    }
+}
---
+++ src/Company/TimeSheets.java
@@ -0,0 +1,8 @@
+package Company;
+
+public interface TimeSheets {
+
+    // creating a method without a body
+    public void fillOutTimeSheets();
+
+}
