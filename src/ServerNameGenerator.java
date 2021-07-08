2875857a085cd88208b8542ea74229d5c844aa75
Sean Lewis <sean.lewis018@gmail.com>
2021-07-08 16:30:07 -0500

    java array practice

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,18 @@
+import java.util.Random;
+
 public class ServerNameGenerator {
+    public static void main(String[] args) {
+        String[] Adjectives = {"Hideous", "Obnoxious", "Panicky", "Putrid", "Smelly", "Ashamed", "Combative", "Homely", "Repulsive", "Unsightly"};
+
+        String[] Nouns = {"Llama", "Blobfish", "Frog", "Banana", "Monkey", "Trashcan", "Einsteins", "Canadian", "SpongeBob", "Squirrel"};
+
+        System.out.println(generator(Adjectives, Nouns));
+        }
+        public static String generator(String[] Adjective, String[] Nouns){
+
+        int adjRndm = new Random().nextInt(Adjective.length);
+        int nonRndm = new Random().nextInt(Nouns.length);
+        return "Your server name is " + Adjective[adjRndm] + " " + Nouns[nonRndm];
+
+    }
 }
