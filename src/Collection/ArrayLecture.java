eb67e01176191df3fbaa3695723b8cf2547d6671
Sean Lewis <sean.lewis018@gmail.com>
2021-07-08 10:59:23 -0500

    java array practice

---
+++ src/Collection/ArrayLecture.java
@@ -0,0 +1,11 @@
+package Collection;
+
+import java.util.Arrays;
+
+public class ArrayLecture {
+    public static void main(String[] args) {
+        int[] intArray = new int [] {1, 2, 3};
+
+        System.out.println(Arrays.toString(intArray));  //[1, 2, 3]
+    }
+}
