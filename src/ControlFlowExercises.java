6334892fe5318729fc4520d2671d5418c4d9f204
Sean Lewis <sean.lewis018@gmail.com>
2021-07-12 14:35:01 -0500

    Java rework

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -20,50 +20,47 @@ public class ControlFlowExercises {
         System.out.println("Enter Numerical Grade: ");
         int number = sc.nextInt();
 
-//        if (number % 5 == 0 && number % 3 ==0) {
-//            System.out.println("FizzBuzz");
-//        } else if (number % 3 == 0) {
-//            System.out.println("Fizz");
-//        } else if (number % 5 == 0) {
-//            System.out.println("Buzz");
-//        } else
-//            System.out.println(number);
-//    }
-
-//        for (int x = 5; x <=15; x++){
-//            System.out.println(x);
-//    }
-//
-//            for (int n = 2; n < 1_000_000; n *= 2) {
-//                System.out.println(n);
-//    }
-
-//        System.out.println("Here is your table!");
-//        System.out.println("number | squared | cubed");
-//        System.out.println("------ | ------- | -----");
-//
-//        for (int y = 1; y <= number; y++) {
-//            System.out.println(y + "       | " + y*y + "         | " + y*y*y);
-//        }
+        if (number % 5 == 0 && number % 3 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 5 == 0) {
+            System.out.println("Buzz");
+        } else
+            System.out.println(number);
 
 
+        for (int x = 5; x <= 15; x++) {
+            System.out.println(x);
+        }
 
-            if (number >= 88){
-                System.out.println("You got an A!");
-            } else if (number >= 80){
-                System.out.println("You got an B!");
-            } else if (number >= 67){
-                System.out.println("You got an C!");
-            } else if (number >= 60){
-                System.out.println("You got an D!");
-            } else
-                System.out.println("You got an F!");
-            }
+        for (int n = 2; n < 1_000_000; n *= 2) {
+            System.out.println(n);
+        }
+
+        System.out.println("Here is your table!");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+
+        for (int y = 1; y <= number; y++) {
+            System.out.println(y + "       | " + y * y + "         | " + y * y * y);
+        }
 
 
-
+        if (number >= 88) {
+            System.out.println("You got an A!");
+        } else if (number >= 80) {
+            System.out.println("You got an B!");
+        } else if (number >= 67) {
+            System.out.println("You got an C!");
+        } else if (number >= 60) {
+            System.out.println("You got an D!");
+        } else
+            System.out.println("You got an F!");
+    }
 
 
 }
+
 
 
--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,12 +1,18 @@
 package grades;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
-
+    Scanner sc = new Scanner(System.in);
     HashMap<String, String> usernames = new HashMap<>();
-
+//    usernames.put()
     public static void main(String[] args) {
+    }
+
+    public static void application() {
+        Scanner sc = new Scanner(System.in);
+
 
         Student Demo = new Student("Noobgamer69420");
         Demo.addGrade(95);
@@ -27,6 +33,54 @@ public class GradesApplication {
         Richard.addGrade(90);
         Richard.addGrade(95);
         Richard.addGrade(100);
+
+
+//        System.out.println("Here are the GitHub usernames of our students:\n"
+//                + .keySet());
+        System.out.println("What student would you like to see more information on?\n");
+        String other = sc.next();
+
+        switch (other) {
+            case "Demo":
+//                System.out.println(
+//                        "Name: " + other + "- GitHub Username: " + Student.get("Demo") + "\n" +
+//                                "Current Average: " + Demo.getGradeAverage());
+                restart();
+            case "Wagner":
+//                System.out.println(
+//                        "Name: " + other + "- GitHub Username: " + Student.get("Wagner") + "\n" +
+//                                "Current Average: " + Wagner.getGradeAverage());
+                restart();
+            case "Sean":
+//                System.out.println(
+//                        "Name: " + other + "- GitHub Username: " + students.get("Sean") + "\n" +
+//                                "Current Average: " +Sean.getGradeAverage());
+                restart();
+            case "Richard":
+//                System.out.println(
+//                        "Name: " + other + "- GitHub Username: " + students.get("Richard") + "\n" +
+//                                "Current Average: " + Richard.getGradeAverage());
+                restart();
+            default:
+                System.out.printf("Sorry, no student found with the GitHub username of %s.", other);
+                throw new IllegalStateException("Unexpected value: " + other);
+        }
+        }
+
+        public static boolean cancel () {
+            System.out.println("Then get out of here");
+            return false;
+        }
+        public static void restart () {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Would you like to see another student?\n");
+            String bigBrother = sc.next();
+            if (bigBrother.equalsIgnoreCase("y")) {
+                application();
+            } else {
+                cancel();
+            }
+
+        }
     }
-}
 
