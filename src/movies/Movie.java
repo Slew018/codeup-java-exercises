cdae8dee9effa5559f1f38bfc8dc8900cd7ff3ee
Sean Lewis <sean.lewis018@gmail.com>
2021-07-12 09:00:57 -0500

    java array prac

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -2,6 +2,26 @@ package movies;
 
 public class Movie {
     public static void main(String[] args) {
+    }
+    private String name, category;
+    public Movie(String name, String category){
+        this.name = name;
+        this.category = category;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,5 +1,64 @@
 package util;
 
+import java.util.Scanner;
+
 public class Input {
 
+    private Scanner scanner;
+    private int number;
+    private double dbl;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+        this.number = number;
+        this.dbl = dbl;
+    }
+
+    public int getInt() {
+        System.out.println("Enter a integer");
+        return this.scanner.nextInt();
+    }
+
+
+    public int getInt(int min, int max) {
+        int Num;
+        do {
+            System.out.printf("enter a integer between %d and %d%n", min, max);
+            Num = scanner.nextInt();
+        } while (Num < min || Num > max);
+        return Num;
+    }
+
+    public String getString() {
+        String message = "hello";
+        return message;
+    }
+
+    public boolean yesNo() {
+        System.out.println("Y or Yes, anything else no: ");
+        String answer = scanner.next().toLowerCase();
+
+        if (answer.equals("y") || answer.equals("yes")) {
+            return true;
+        }
+        return false;
+    }
+
+    public double getDbl(double min, double max) {
+        this.dbl = getDbl();
+
+        if (dbl < min) {
+            System.out.println("Pick a number between " + min + " and " + max);
+            getDbl(min, max);
+        } else if (dbl > max) {
+            System.out.println("Pick a number between " + min + " and " + max);
+            getDbl(min, max);
+        }
+        return this.dbl;
+    }
+
+    public double getDbl() {
+        double dbl = scanner.nextDouble();
+        return dbl;
+    }
 }
