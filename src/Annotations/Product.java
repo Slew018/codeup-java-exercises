7ae86288448233334fd25a4bb8b8a1f742cae896
Sean Lewis <sean.lewis018@gmail.com>
2021-07-13 10:31:04 -0500

    java work

---
+++ src/Annotations/Product.java
@@ -0,0 +1,17 @@
+package Annotations;
+
+
+// @Entity
+//public class Product {
+//    //define property name
+//    @Persistent
+//protected String name = null;
+//
+//public String getName(){
+//    return name;
+//}
+//
+//public void setName(name){
+//    this.name = name;
+//    }
+//}
