83334a7d140b3b94533334afac0ebe8048ed0c34
Sean Lewis <sean.lewis018@gmail.com>
2021-07-15 11:06:24 -0500

    java warmupz

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,34 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
+int min = 0;
+int max = 50;
 
-        ArrayList<Integer> myArrList = new ArrayList<Integer>();
-        myArrList.get(2);
+String AlphaNumeric = "Oberon is the best cohort ever!";
+
+//StringBuilder sb = new StringBuilder(n);
+//
+//        for (int i = 0; i < n; i++) {
+//
+//        }
+
+//        int random_int = (int)Math.floor(Math.random()*(max-min+1)+min);
+//        System.out.println(random_int);
+
     }
+//    public static void canBank(int age, int deposit){
+//        Scanner sc = new Scanner(System.in);
+//        int userAge = sc.nextInt();
+//
+//        System.out.println("Enter your age and available funds to deposit");
+//
+//        if(userAge.hasNextInt){
+//
+//        }
+
+//    }
 }
 
+//- replace getInt and getDouble methods and instead we’re using the result of getString and checking to see if its a double or an int and then throwing that into a try / catch  and if an exception gets thrown, it’s handled in the catch block
